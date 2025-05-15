503c0fb94449bc08b8927c427c7414b168d0976c
Leehyundong <hyundongl@icloud.com>
2025-05-15 10:13:00 +0900

    5/14일까지 크롤링 완료

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # news-sentiment-analysis
 
-1. docker-compose up -d로 실행
-2. docker exec -it data_science_introduction_mysql mysql -u root -p1234 news_sentiment_db로 db 접속 가능
+### 1. 컨테이너 실행
+
+docker-compose up -d
+
+### 2. 데이터 복원
+
+docker exec -i data_science_introduction_mysql mysql -u root -p1234 news_sentiment_db < backups/backup_20250515.sql
+
+### 3. mysql 접속
+
+docker exec -it data_science_introduction_mysql mysql -u root -p1234 news_sentiment_db
--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE news_articles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    article_id VARCHAR(20) PRIMARY KEY,
     title VARCHAR(500),
     summary TEXT,
     publisher VARCHAR(100),
@@ -10,14 +10,13 @@ CREATE TABLE news_articles (
 );
 
 CREATE TABLE news_comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    article_id INT,
+    comment_id VARCHAR(20) PRIMARY KEY,  -- 수동 입력
+    article_id VARCHAR(20),
     nickname VARCHAR(100),
     content TEXT,
     like_count INT,
     dislike_count INT,
     comment_date DATETIME,
     collected_at DATETIME,
-    FOREIGN KEY (article_id) REFERENCES news_articles(id),
-    FOREIGN KEY (parent_comment_id) REFERENCES news_comments(id)
+    FOREIGN KEY (article_id) REFERENCES news_articles(article_id)
 );
